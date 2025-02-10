151eac833fc5a1de4ae1d49df0a1f9ed597a7e12
armohdbd <88308893+armohdbd@users.noreply.github.com>
2025-02-10 11:39:10 +0800

    Remove unused Next.js Image import from AnimatedHero component

--- mhtc-web/components/AnimatedHero.tsx
+++ mhtc-web/components/AnimatedHero.tsx
@@ -3,7 +3,6 @@
 import { motion } from "framer-motion";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import Image from "next/image";
 
 export default function AnimatedHero() {
   const fadeInUp = {
